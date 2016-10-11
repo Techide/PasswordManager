cc0d731347b105240ec588432ee661566116e2c1
Danny Johansen <techide@gmail.com>
2016-10-12 00:51:57 +0200

    ViewModelLocator Attempt

---
+++ PasswordManager/Logic/IViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace PasswordManager.Logic {
+    public interface IViewModel<TModel> where TModel : UserControl {
+    }
+}
--- PasswordManager/Logic/ViewModelLocator.cs
+++ PasswordManager/Logic/ViewModelLocator.cs
@@ -1,14 +1,29 @@
-﻿using SimpleInjector;
+﻿using PasswordManager.Logic.Profile;
+using PasswordManager.Presentation;
+using SimpleInjector;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PasswordManager.Logic {
     public class ViewModelLocator {
         private static Container _ioc;
+
         public ViewModelLocator() {
+            Bootstrap();
+        }
+
+        public ProfileListViewModel ProfileList { get { return _ioc.GetInstance<ProfileListViewModel>(); } }
+
+        private static void Bootstrap() {
+            var container = new Container();
+
+            container.Register(typeof(IViewModel<>), new[] { typeof(IViewModel<>).GetTypeInfo().Assembly });
+
+            _ioc = container;
 
         }
     }
